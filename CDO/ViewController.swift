ddc651b8009fb838ae8df7b5d3a21dac150fe84b
Jon Alaniz <jonalaniz@gmail.com>
2025-10-13 23:45:18 -0500

    Initial Commit

---
+++ CDO/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CDO
+//
+//  Created by Jon Alaniz on 10/13/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

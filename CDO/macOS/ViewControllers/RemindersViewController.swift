6d09d8b528cd19ade51d680668e95d76f1bc3c63
Jon Alaniz <jonalaniz@gmail.com>
2025-10-20 23:48:57 -0500

    Added RemindersViewController

--- CDO/macOS/ViewControllers/RemindersViewController.swift
+++ CDO/macOS/ViewControllers/RemindersViewController.swift
@@ -5,4 +5,30 @@
 //  Created by Jon Alaniz on 10/19/25.
 //
 
-import Foundation
+import Cocoa
+
+class RemindersViewController: NSViewController {
+    @IBOutlet weak var tableView: NSTableView!
+
+    private let manager = RemindersManager.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = manager
+        tableView.delegate = manager
+        manager.delegate = self
+        manager.fetchReminders()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
+extension RemindersViewController: DataManagerDelegate {
+    func didUpdateClients() {
+        tableView.reloadData()
+    }
+}

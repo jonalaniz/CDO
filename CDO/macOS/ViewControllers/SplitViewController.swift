ed12ac3858d32dd07ffa2a9146013c348154ca55
Jon Alaniz <jonalaniz@gmail.com>
2025-10-21 01:08:26 -0500

    Linting

---
+++ CDO/macOS/ViewControllers/SplitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewController.swift
+//  CDO
+//
+//  Created by Jon Alaniz on 10/21/25.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

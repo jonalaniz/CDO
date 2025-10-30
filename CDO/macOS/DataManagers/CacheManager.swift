93707c07c3b8e92c5277052a6b1f8f14c865df55
Jon Alaniz <jonalaniz@gmail.com>
2025-10-29 19:58:41 -0500

    Initial CacheManager addition

---
+++ CDO/macOS/DataManagers/CacheManager.swift
@@ -0,0 +1,13 @@
+//
+//  CacheManager.swift
+//  CDO
+//
+//  Created by Jon Alaniz on 10/29/25.
+//
+
+import Foundation
+
+class CacheManager {
+    static let shared = CacheManager()
+    private init() {}
+}

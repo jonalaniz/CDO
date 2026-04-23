04e30ccdc7b9183de11bef956084201a15e65750
Jon Alaniz <jonalaniz@gmail.com>
2026-04-23 13:46:59 -0500

    removed old classes

---
+++ CDO/macOS/Coordinators/MainCoordinator+NSToolbarDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator+NSToolbarDelegate.swift
+//  CDO
+//
+//  Created by Jon Alaniz on 4/23/26.
+//
+
+import Foundation
---
+++ CDO/macOS/MainWindow/MainSplitView.swift
@@ -0,0 +1,42 @@
+//
+//  MainSplitView.swift
+//  CDO
+//
+//  Created by Jon Alaniz on 10/21/25.
+//
+
+import Cocoa
+
+final class MainSplitView: NSSplitViewController {
+    private let sidebarItem: NSSplitViewItem
+    private var contentItem: NSSplitViewItem
+
+    init(sidebar: NSViewController, content: NSViewController) {
+        sidebarItem = NSSplitViewItem(
+            sidebarWithViewController: sidebar
+        )
+        contentItem = NSSplitViewItem(
+            viewController: content
+        )
+        super.init(nibName: nil, bundle: nil)
+        setupSplitView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupSplitView() {
+        sidebarItem.minimumThickness = 190
+        sidebarItem.maximumThickness = 220
+        splitViewItems = [sidebarItem, contentItem]
+
+    }
+
+    func setContentItem(_ viewController: NSViewController) {
+        let splitViewItem = NSSplitViewItem(viewController: viewController)
+        removeSplitViewItem(contentItem)
+        addSplitViewItem(splitViewItem)
+        contentItem = splitViewItem
+    }
+}
